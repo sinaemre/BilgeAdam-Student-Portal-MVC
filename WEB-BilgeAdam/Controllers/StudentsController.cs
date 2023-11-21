e20a6b475bc6550a13e6ca0c58f92bdc0b9116cf
sinaemre <snabkr7010@gmail.com>
2023-11-21 11:07:38 +0300

    21-11-2023-(1.Part)

---
+++ WEB-BilgeAdam/Controllers/StudentsController.cs
@@ -0,0 +1,43 @@
+﻿using AutoMapper;
+using Infrastructure_BilgeAdam.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WEB_BilgeAdam.Models.ViewModels;
+
+namespace WEB_BilgeAdam.Controllers
+{
+    public class StudentsController : Controller
+    {
+        private readonly IStudentRepository _studentRepo;
+        private readonly IMapper _mapper;
+
+        public StudentsController(IStudentRepository studentRepo, IMapper mapper)
+        {
+            _studentRepo = studentRepo;
+            _mapper = mapper;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var students = await _studentRepo.GetFilteredList
+                (
+                    select: x => new StudentVM
+                    { 
+                        Id = x.Id,
+                        StudentNo = x.StudentNo,
+                        FirstName = x.FirstName,
+                        LastName = x.LastName,
+                        BirthDate = x.BirthDate,
+                        ClassroomNo = x.Classroom.ClassroomNo,
+                        CreatedDate = x.CreatedDate,
+                        UpdatedDate = x.UpdatedDate
+                    },
+                    where: x => x.Status != ApplicationCore_BilgeAdam.Entities.Abstract.Status.Passive,
+                    orderBy: x => x.OrderByDescending(z => z.CreatedDate),
+                    join: x=> x.Include(z => z.Classroom)
+                );
+
+            return View(students);
+        }
+    }
+}
---
+++ WEB-BilgeAdam/Models/ViewModels/StudentVM.cs
@@ -0,0 +1,14 @@
+﻿namespace WEB_BilgeAdam.Models.ViewModels
+{
+    public class StudentVM
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime? UpdatedDate { get; set; }
+        public int StudentNo { get; set; }
+        public byte ClassroomNo { get; set; }
+    }
+}
